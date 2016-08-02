a11a7feca667bd5cfa8c6acae032eabbe93ae751
Daniel Yoo <yoo.d93@gmail.com>
2016-08-02 03:30:55 -0400

    GameBoard class made

---
+++ GameBoard.php
@@ -0,0 +1,12 @@
+<?php
+
+class GameBoard
+{
+    public function initialize($playerOne, $playerTwo)
+    {
+        echo 'Game has begun! ' . $playerOne . ' VS ' . $playerTwo;
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 
 include 'DB_connect.php';
+include 'GameBoard.php';
 
 	$token = $_POST['token'];
 
-	echo $token;
+	//make sure the request comes from Slack
+	if ($token != 'tywWH21kkZOVWWB7tGQLbbzc') {
+		echo 'What are you doing here >:( I only accept requests from Slack!';
+	}
+
+	//initializing phase
+
+	$playerOne = $_POST['user_name'];
+	$playerTwo = $_POST['text'];
+	$gameBoard = new GameBoard();
+	$gameBoard->initialize($playerOne, $playerTwo);
 
 	$db = new DB_connect();
 	$db->connect();
