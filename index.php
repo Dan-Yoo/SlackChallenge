f179b2adbb5da7b2a607c1804065765707dcbd81
Daniel Yoo <yoo.d93@gmail.com>
2016-08-02 01:32:09 -0400

    db connection settings

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 	echo 'hello';
 
-	# This function reads your DATABASE_URL configuration automatically set by Heroku
-	# the return value is a string that will work with pg_connect
+	// # This function reads your DATABASE_URL configuration automatically set by Heroku
+	// # the return value is a string that will work with pg_connect
 	function pg_connection_string() {
 	  return "dbname=d2e1gs12pvg3so host=ec2-54-243-249-137.compute-1.amazonaws.com port=5432 user=almwanyyyjcqvw password=2vUmD01T4vMeT768uOHk61Cfyc sslmode=require";
 	}
  
-	# Establish db connection
-	$db = pg_connect(pg_connection_string());
-	if (!$db) {
-	    echo "Database connection error."
-	    exit;
-	}
+	// # Establish db connection
+	// $db = pg_connect(pg_connection_string());
+	// if (!$db) {
+	//     echo "Database connection error."
+	//     exit;
+	// }
  
-	$result = pg_query($db, "SELECT statement goes here");
+	// $result = pg_query($db, "SELECT statement goes here");
 
 ?>
