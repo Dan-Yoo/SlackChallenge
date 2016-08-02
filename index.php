95a1b563faafac5d41d946499e493a0250378fb8
Daniel Yoo <yoo.d93@gmail.com>
2016-08-02 01:33:09 -0400

    db connection testing

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
 	  return "dbname=d2e1gs12pvg3so host=ec2-54-243-249-137.compute-1.amazonaws.com port=5432 user=almwanyyyjcqvw password=2vUmD01T4vMeT768uOHk61Cfyc sslmode=require";
 	}
  
-	// # Establish db connection
-	// $db = pg_connect(pg_connection_string());
-	// if (!$db) {
-	//     echo "Database connection error."
-	//     exit;
-	// }
+	# Establish db connection
+	$db = pg_connect(pg_connection_string());
+	if (!$db) {
+	    echo "Database connection error."
+	    exit;
+	}
+
+	echo "connected successfully!";
  
 	// $result = pg_query($db, "SELECT statement goes here");
 
