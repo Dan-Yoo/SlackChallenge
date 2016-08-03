78eb487f1d08b80ab31475921c34d38cd44ab35e
Daniel Yoo <yoo.d93@gmail.com>
2016-08-02 21:31:18 -0400

    query testing

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'DB_connect.php';
-include 'GameBoard.php';
+include 'TicTacToeController.php';
 
 	$token = $_POST['token'];
 
@@ -10,9 +10,21 @@ include 'GameBoard.php';
 		die("What are you doing here >:( I only accept requests from Slack!");
 	}
 
+	//connect to database
 	$db = new DB_connect();
 	$connection = $db->connect();
 	
+	//check if channel id exists in db. If it doesnt, initialize one!
+	$query = "SELECT id FROM public.tictactoe WHERE channel_id = 111";
+	$result = pg_query($connection, $query);
+
+	if (!pg_fetch_row($result)) {
+		die('no results foudn');
+	}
+
+	die('there was a result!')
+
+
 	$result = pg_query($connection, "SELECT * FROM public.tictactoe");
 
 	while ($row = pg_fetch_row($result)) {
@@ -26,8 +38,8 @@ include 'GameBoard.php';
 	$playerTwo = $_POST['text'];
 	$channelId = $_POST['channel_id'];
 
-	$gameBoard = new GameBoard();
-	$gameBoard->initialize($playerOne, $playerTwo);
+	// $gameBoard = new GameBoard();
+	// $gameBoard->initialize($playerOne, $playerTwo);
 
 
 
