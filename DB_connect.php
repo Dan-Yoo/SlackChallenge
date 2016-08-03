5a947499f66746b2920ec5c5f8e470c0290761b6
Daniel Yoo <yoo.d93@gmail.com>
2016-08-03 02:53:18 -0400

    test

--- DB_connect.php
+++ DB_connect.php
@@ -13,9 +13,9 @@ class DB_Connect
         $db = pg_connect( "$host $port $dbname $credentials"  );
 
         if(!$db){
-           echo "Error : Unable to open database\n";
+           //echo "Error : Unable to open database\n";
         } else {
-           echo "Opened database successfully\n";
+           //echo "Opened database successfully\n";
         }
         return $db;
     }
