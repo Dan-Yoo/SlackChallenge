971fb40bcb895c5a39686ab7881fdcf96a06bdf1
Daniel Yoo <yoo.d93@gmail.com>
2016-08-02 22:44:09 -0400

    testing

--- TicTacToeController.php
+++ TicTacToeController.php
@@ -31,18 +31,18 @@ class TicTacToeController
 	 */
 	public function initializeGame($connection, $playerOne, $playerTwo, $channelId)
 	{
-		$row1 		= array('column1' => '', 'column2' => '', 'column3' => '');
-		$row2 		= array('column1' => '', 'column2' => '', 'column3' => '');
-		$row3 		= array('column1' => '', 'column2' => '', 'column3' => '');
-		$board 		= array('row1' => $row1,'row2' => $row2, 'row3' => $row3);
-		$jsonBoard 	= json_encode($board);
+		// $row1 		= array('column1' => '', 'column2' => '', 'column3' => '');
+		// $row2 		= array('column1' => '', 'column2' => '', 'column3' => '');
+		// $row3 		= array('column1' => '', 'column2' => '', 'column3' => '');
+		// $board 		= array('row1' => $row1,'row2' => $row2, 'row3' => $row3);
+		// $jsonBoard 	= json_encode($board);
 
 		$row = array(
 			'player_1' => $playerOne,
 			'player_2' => $playerTwo,
 			'channel_id' => $channelId,
 			'turn' => 1,
-			'board' => $jsonBoard
+			'board' => "{}"
 		);
 
 		pg_insert($connection, 'public.tictactoe', $row);
