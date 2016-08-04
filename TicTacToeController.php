82a31232972523374ac1c0d80560e5602f42484a
Daniel Yoo <yoo.d93@gmail.com>
2016-08-04 01:28:10 -0400

    implementing first roudn of validation for existing user in intialize

--- TicTacToeController.php
+++ TicTacToeController.php
@@ -56,11 +56,11 @@ class TicTacToeController
 		//validate that player 2 is indeed a user in the current channel
 		//check if playerTwo is in the member list. if yes, get the id. if not return false.
 		$members = HttpHelper::getMembersList();
-		//$playerTwoId = $this::getPlayerId($members, $playerTwo)
+		$playerTwoId = $this::getPlayerId($members, $playerTwo)
 
-		// if (empty($playerTwoId)) {
-		// 	return HttpHelper::genericResponse("There is no such user in this channel!");
-		// }
+		if (empty($playerTwoId)) {
+			return HttpHelper::genericResponse("There is no such user in this channel!");
+		}
 
 		// if (!$this::validatePlayerIsInChannel($playerTwoId)) {
 		// 	return HttpHelper::genericResponse("There is no such user in this channel!");
