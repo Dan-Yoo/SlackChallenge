3b9ccb8656c3f06e1614c9aa9ab920d43c06f607
Daniel Yoo <yoo.d93@gmail.com>
2016-08-05 00:11:38 -0400

    final code cleaning, hiding token

--- DB_connect.php
+++ DB_connect.php
@@ -18,10 +18,9 @@ class DB_Connect
         $db = pg_connect( "$host $port $dbname $credentials"  );
 
         if(!$db){
-           //echo "Error : Unable to open database\n";
-        } else {
-           //echo "Opened database successfully\n";
-        }
+           echo "Error : Unable to open database\n";
+        } 
+        
         return $db;
     }
 
